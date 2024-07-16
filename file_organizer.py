c7633d7b68b60aeb6377458c67c50867a10366ea
souvik debbarma <souvikdbarma23@gmail.com>
2024-07-17 00:34:04 +0530

    wrapped the code in a function for better modularity.

--- file_organizer.py
+++ file_organizer.py
@@ -1,15 +1,26 @@
 import os
 import shutil
 
-path = input("enter path: ")
-files = os.listdir(path)
+def organize_files(path):
+    files = os.listdir(path)
 
-for file in files:
-    filename,extension = os.path.splitext(file)
-    extension = extension[1:]
-    
-    if os.path.exists(path+'/'+extension):
-        shutil.move(path+'/'+file, path+'/'+extension+'/'+file)
+    for file in files:
+        filename, extension = os.path.splitext(file)
+        extension = extension[1:]  # Remove the leading dot from the extension
+
+        if extension:  # Check if the file has an extension
+            ext_path = os.path.join(path, extension)
+            file_path = os.path.join(path, file)
+
+            if not os.path.exists(ext_path):
+                os.makedirs(ext_path)
+            
+            shutil.move(file_path, os.path.join(ext_path, file))
+
+if __name__ == "__main__":
+    path = input("Enter path: ")
+    if os.path.isdir(path):
+        organize_files(path)
+        print("Files organized successfully.")
     else:
-        os.makedirs(path+'/'+extension)
-        shutil.move(path+'/'+file, path+'/'+extension+'/'+file)
+        print("Invalid path. Please enter a valid directory path.")
